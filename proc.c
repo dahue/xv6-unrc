65def79866b9a0c64aaee795337eb84c3728940c
Dahue Adrian Tissera <adriantissera@gmail.com>
2021-09-16 06:04:58 +0100

    fork uses the new version of copyuvm

--- proc.c
+++ proc.c
@@ -180,7 +180,7 @@ growproc(int n)
 int
 fork(void)
 {
-  int i, pid, cond;
+  int i, pid;
   struct proc *np;
   struct proc *curproc = myproc();
 
@@ -190,11 +190,11 @@ fork(void)
   }
 
   // Copy process state from proc.
-  if (strncmp(curproc->name, "init", sizeof(curproc->name)) == 0)
-    cond = (np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0;
-  else
-    cond = (np->pgdir = copyuvm2(curproc->pgdir, curproc->sz)) == 0;
-  if(cond){
+  // if (strncmp(curproc->name, "init", sizeof(curproc->name)) == 0)
+  //   cond = (np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0;
+  // else
+  //   cond = (np->pgdir = copyuvm2(curproc->pgdir, curproc->sz)) == 0;
+  if( (np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0 ){
     kfree(np->kstack);
     np->kstack = 0;
     np->state = UNUSED;
--- vm.c
+++ vm.c
@@ -312,40 +312,40 @@ clearpteu(pde_t *pgdir, char *uva)
 
 // Given a parent process's page table, create a copy
 // of it for a child.
+// pde_t*
+// copyuvm(pde_t *pgdir, uint sz)
+// {
+//   pde_t *d;
+//   pte_t *pte;
+//   uint pa, i, flags;
+//   char *mem;
+
+//   if((d = setupkvm()) == 0)
+//     return 0;
+//   for(i = 0; i < sz; i += PGSIZE){
+//     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+//       panic("copyuvm: pte should exist");
+//     if(!(*pte & PTE_P))
+//       panic("copyuvm: page not present");
+//     pa = PTE_ADDR(*pte);
+//     flags = PTE_FLAGS(*pte);
+//     if((mem = kalloc()) == 0)
+//       goto bad;
+//     memmove(mem, (char*)P2V(pa), PGSIZE);
+//     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+//       kfree(mem);
+//       goto bad;
+//     }
+//   }
+//   return d;
+
+// bad:
+//   freevm(d);
+//   return 0;
+// }
+
 pde_t*
 copyuvm(pde_t *pgdir, uint sz)
-{
-  pde_t *d;
-  pte_t *pte;
-  uint pa, i, flags;
-  char *mem;
-
-  if((d = setupkvm()) == 0)
-    return 0;
-  for(i = 0; i < sz; i += PGSIZE){
-    if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
-      panic("copyuvm: pte should exist");
-    if(!(*pte & PTE_P))
-      panic("copyuvm: page not present");
-    pa = PTE_ADDR(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto bad;
-    memmove(mem, (char*)P2V(pa), PGSIZE);
-    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
-      kfree(mem);
-      goto bad;
-    }
-  }
-  return d;
-
-bad:
-  freevm(d);
-  return 0;
-}
-
-pde_t*
-copyuvm2(pde_t *pgdir, uint sz)
 {
   pde_t *d;
   pte_t *pte;
@@ -363,12 +363,13 @@ copyuvm2(pde_t *pgdir, uint sz)
     flags = PTE_FLAGS(*pte);
     if(mappages(d, (void*)i, PGSIZE, pa, flags) < 0) {
       freevm(d);
+      // reload cr3 (Translation Look-aside Buffer)
       lcr3(V2P(pgdir));
       return 0;
     }
     inc_refpgcount(pa);
   }
-  lcr3(V2P(pgdir));   // reload cr3 (Translation Look-aside Buffer)
+  lcr3(V2P(pgdir));
   return d;
 }
 
