cd92dad5ae426372c18bf53a7618d193d2b974fb
Dahue Adrian Tissera <adriantissera@gmail.com>
2021-12-20 15:25:10 -0300

    bugfixing

--- defs.h
+++ defs.h
@@ -184,12 +184,11 @@ void            freevm(pde_t*);
 void            inituvm(pde_t*, char*, uint);
 int             loaduvm(pde_t*, char*, struct inode*, uint, uint);
 pde_t*          copyuvm(pde_t*, uint);
-pde_t*          copyuvm2(pde_t*, uint);
 void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
-void 			cowhandler(uint);
+void            cowhandler(uint);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
